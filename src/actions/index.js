888fc148c150b7621b17625cc8ff202e1549c74c
Harry <hjwjennings@gmail.com>
2019-09-04 15:12:37 +0100

    added project to Github

---
+++ src/actions/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import jsonPlaceholder from "../api/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+	
+	// WITHOUT CHAIN
+	// const userIds = _.uniq(_.map(getState().posts, 'userId'))
+	// userIds.forEach(id => dispatch(fetchUser(id)));
+
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach(id => dispatch(fetchUser(id)))
+		.value()
+}
+
+export const fetchPosts = () => async dispatch => {
+	const response = await jsonPlaceholder.get("/posts");
+
+	dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`)
+
+	dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+
+// MEMOIZED VERSION
+// export const fetchUser = (id) => dispatch => {
+// 	_fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+
+// 	const response = await jsonPlaceholder.get(`/users/${id}`)
+
+// 	dispatch({ type: "FETCH_USER", payload: response.data });
+
+// });
